7a9327f3f7588a799b5a744b451b25ebfa270bf3
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-18 00:05:44 +0530

    global css running

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-danger">Next js portfolio App</h1>
+    <section className="homepage">
+      <div className="container-fluid">
+
+      </div>
+    </section>
     </>
     
   );
