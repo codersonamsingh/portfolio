c6661fa0001f09fed14a19fec20e38f1aec110ec
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-21 18:17:59 +0530

    Page.js run

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-
+import Image from 'next/image';
+import userImage from '../asserts/images/img.jpg';
 
 export default function Home() {
   return (
@@ -8,8 +9,20 @@ export default function Home() {
         <div className="row align-items-center wrapper">
           <div className="col-md-4">
             <div className="imgWrap">
+              <Image src={userImage}/>
 
             </div>
+          </div>
+          <div className="col-md-8">
+            <h1 className='text-white font-weight-bold'><span className='text-common d-block'>I'M STEVE MILNER.</span>
+              WEB DESIGNER
+            </h1>
+
+            <p className='text-white'>I'm a Tunisian based web designer & front-end developer focused on crafting clean
+              & user friendly experiences, I am passionate about building excellent software that improve the lives
+              of those around me.
+            </p>
+
           </div>
         </div>
 
