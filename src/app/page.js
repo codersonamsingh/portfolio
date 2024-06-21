8acbcbd53513d6bacffda0ec3364376d8b66e605
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-21 16:44:35 +0530

    global css running

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,15 @@
 export default function Home() {
   return (
     <>
-    <section className="homepage">
+    <section className="homePage">
       <div className="container-fluid">
+        <div className="row align-items-center wrapper">
+          <div className="col-md-4">
+            <div className="imgWrap">
+
+            </div>
+          </div>
+        </div>
 
       </div>
     </section>
