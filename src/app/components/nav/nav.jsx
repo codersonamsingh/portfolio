9d277cf16d9b470107647c7acaa03e1d39b56c50
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-25 16:04:57 +0530

    nav.css running

--- src/app/components/nav/nav.jsx
+++ src/app/components/nav/nav.jsx
@@ -1,5 +1,6 @@
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Button from '@mui/material/Button';
+import './nav.css';
 
 const Nav=()=>{
     return(
@@ -7,7 +8,9 @@ const Nav=()=>{
         <nav>
             <ul>
                 <li>
-                <Button className='btn-common iconBtn'>More About Me <span className='icon d-flex align-item-center 
+                <Button className='btn-common iconBtn'>
+                    <span className="title">More About Me</span>
+                     <span className='icon d-flex align-item-center 
             justify-content-center'><ArrowForwardIcon/></span></Button>
                 </li>
             </ul>
