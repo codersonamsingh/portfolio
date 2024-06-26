2e5d972cedf5cb710f6140a734d735a43de40a5a
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-26 18:20:54 +0530

    nav.jsx running

--- src/app/components/nav/nav.jsx
+++ src/app/components/nav/nav.jsx
@@ -5,6 +5,7 @@ import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
 import MarkunreadOutlinedIcon from '@mui/icons-material/MarkunreadOutlined';
 import './nav.css';
+import Link from 'next/link';
 
 const Nav=()=>{
     return(
@@ -12,32 +13,36 @@ const Nav=()=>{
         <nav>
             <ul>
                 <li className='btn-list'>
+                    <Link href="/">
                 <Button className='btn-common iconBtn'>
                     <span className="title">Home</span>
                      <span className='icon d-flex align-item-center 
             justify-content-center'><HomeOutlinedIcon/></span></Button>
-            
+            </Link>
                 </li>
               <li className='btn-list'>
+              <Link href="/">
                 <Button className='btn-common iconBtn'>
                     <span className="title">About Me</span>
                      <span className='icon d-flex align-item-center 
             justify-content-center'><Person2OutlinedIcon/></span></Button>
-            
+             </Link>
                 </li>
                 <li>
+                <Link href="/">
                 <Button className='btn-common iconBtn'>
                     <span className="title">Portfolio</span>
                      <span className='icon d-flex align-item-center 
             justify-content-center'><BusinessCenterOutlinedIcon/></span></Button>
-            
+             </Link>
                 </li>
                 <li>
+                <Link href="/">
                 <Button className='btn-common iconBtn'>
                     <span className="title">Contact</span>
                      <span className='icon d-flex align-item-center 
             justify-content-center'><MarkunreadOutlinedIcon/></span></Button>
-            
+             </Link>
                 </li>
                 
             </ul>
