7f415a88c5da6fe0fd039028d8e66ecbd718630b
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-26 18:06:54 +0530

    page.js running

--- src/app/components/nav/nav.jsx
+++ src/app/components/nav/nav.jsx
@@ -1,6 +1,9 @@
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 import Button from '@mui/material/Button';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import MarkunreadOutlinedIcon from '@mui/icons-material/MarkunreadOutlined';
 import './nav.css';
 
 const Nav=()=>{
@@ -17,25 +20,26 @@ const Nav=()=>{
                 </li>
               <li className='btn-list'>
                 <Button className='btn-common iconBtn'>
-                    <span className="title">H2</span>
+                    <span className="title">About Me</span>
                      <span className='icon d-flex align-item-center 
-            justify-content-center'><HomeOutlinedIcon/></span></Button>
+            justify-content-center'><Person2OutlinedIcon/></span></Button>
             
                 </li>
                 <li>
                 <Button className='btn-common iconBtn'>
-                    <span className="title">Home</span>
+                    <span className="title">Portfolio</span>
                      <span className='icon d-flex align-item-center 
-            justify-content-center'><HomeOutlinedIcon/></span></Button>
+            justify-content-center'><BusinessCenterOutlinedIcon/></span></Button>
             
                 </li>
                 <li>
                 <Button className='btn-common iconBtn'>
-                    <span className="title">Home</span>
+                    <span className="title">Contact</span>
                      <span className='icon d-flex align-item-center 
-            justify-content-center'><HomeOutlinedIcon/></span></Button>
+            justify-content-center'><MarkunreadOutlinedIcon/></span></Button>
             
                 </li>
+                
             </ul>
             </nav>
             
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+"use client"
 import Image from 'next/image';
 import userImage from '../asserts/images/img.jpg';
 import Button from '@mui/material/Button';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import Typed from 'typing-ind'
+import { TypeAnimation } from 'react-type-animation';
+
 
 
 export default function Home() {
@@ -20,14 +24,41 @@ export default function Home() {
           <div className="col-md-8">
             <div className='d-flex'>
               <span className='line mr-4'></span>
-            <h1 className='text-white font-weight-bold mt-0'><span className='text-common d-block'>I'M SONAM SINGH.</span>
-              WEB DESIGNER
-            </h1></div>
+            <h1 className='text-white font-weight-bold mt-0'><span className='text-common d-block'>
+              I'M SONAM SINGH.</span>
+              <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'WEB DESIGNER',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'WEB DEVELOPER',
+        1000,
+        'REACT DEVELOPER',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ display: 'inline-block' }}
+      repeat={Infinity}
+    />
+            </h1>
+            {/* <h1>
+            <Typed 
+      type={['Test ', 'One', 'Two', 'Three']} // if no backspace animation just write 1 string only (no need to use array)
+      backspace={[1, 2]} // contains the index value of the type property, if only 1 index no need to use array (will execute from the smallest index)
+      delay={1000} // delay backspace animation (in milliseconds)
+      duration={50} // duration of typing animation per character (in milliseconds). Default 100
+      cursor={'blinking-text-cursor'} // style for blinking text cursor animation. can be filled directly with css properties or class names in your css file
+      />
+      </h1> */}
+            </div>
+
 
             <p className='text-white'>I'm a Tunisian based web designer & front-end developer focused on crafting clean
               & user friendly experiences, I am passionate about building excellent software that improve the lives
               of those around me.
             </p>
+            <br/>
 
              <div className='d-flex align items center'>
             <Button className='btn-common iconBtn'>More About Me <span className='icon d-flex align-item-center 
