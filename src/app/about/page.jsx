5a1a432494810038051009c3382c14f488225ecb
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-26 23:27:09 +0530

    about pag running

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,13 @@
 const About=()=>{
 return (
     <section className="aboutPage">
-      <div className="container-fluid"></div>
+      <div className="container-fluid">
+       <div className="d-flex">
+       <h1 className="hd text-center m-auto">ABOUT <span>ME</span>
+        <span className="title-bg">RESUME</span>
+        </h1>
+       </div>
+      </div>
       </section>
 )
 
