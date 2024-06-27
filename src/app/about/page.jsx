644bbf21be07d2b2f2b556a2103a3d523f1d1ec9
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-27 17:48:52 +0530

    global.css run

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,5 @@
+import Button from '@mui/material/Button';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 const About=()=>{
 return (
     <section className="aboutPage">
@@ -7,6 +9,40 @@ return (
         <span className="title-bg">RESUME</span>
         </h1>
        </div>
+
+      <div className="row mt-5 aboutWrapper">
+        <div className="col part1">
+          <h2 className="text-white">PERSONAL INFO</h2>
+          <div className="row">
+            <div className="col">
+              <h4>First Name : <span>Sonam</span></h4>
+              <h4>last Name : <span>Singh</span></h4>
+              <h4>Age : <span>20</span></h4>
+              <h4>Nationality : <span>INDIAN</span></h4>
+              <h4>Freelance : <span>Available</span></h4>
+            </div>
+    
+
+            <div className="col">
+              <h4>Address : <span>India</span></h4>
+              <h4>Phone: <span>+91 9572965040</span></h4>
+              <h4>Email: <span className="smm">sonam.sin.rajput@gmail.com</span></h4>
+              <h4>WhatsApp: <span>+91 9430687559</span></h4>
+              <h4>Langages  : <span>English, Hindi</span></h4>
+            </div>
+
+
+          </div>
+          <Button className='btn-common iconBtn'>Download CV <span className='icon d-flex align-item-center 
+            justify-content-center'><FileDownloadOutlinedIcon/></span></Button>
+            
+        </div>
+        <div className="col part2">
+
+        </div>
+        
+      </div>
+
       </div>
       </section>
 )
