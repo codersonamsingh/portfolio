a50e377054ee89b1a18e5368b43ed128d3ecf2c5
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-06-30 23:08:26 +0530

    portfolio running

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+const Portfolio = () => {
+    return(
+        <section className="portfolioPage">
+      <div className="container-fluid">
+            <div className="d-flex">
+              <h1 className="hd text-center m-auto">My <span>Portfolio</span>
+             <span className="title-bg">WORKS</span>
+             </h1>
+         </div>
+
+        
+             <div className="wrapper mt-5">
+                <div className="box">
+                    <Image 
+                          src="/Image/qualifier.png"
+                          alt="Image"
+                width={355}
+                height={180}
+                    />
+                </div>
+
+                <div className="box">
+
+                <Image 
+                          src="/Image/ASNit.png"
+                          alt="Image"
+                width={355}
+                height={180}
+                    />
+                    
+                </div>
+
+                <div className="box">
+                <Image 
+                          src="/Image/Softechinfra 2.png"
+                          alt="Image"
+                width={355}
+                height={180}
+                    />
+                </div>
+
+                <div className="box">
+                <Image 
+                          src="/Image/lifesaviour.png"
+                          alt="Image"
+                width={355}
+                height={180}
+                    />
+                </div>
+                <div className="box">
+                <Image 
+                          src="/Image/Softechinfra.png"
+                          alt="Image"
+                width={355}
+                height={180}
+                    />
+                </div>
+                
+
+                <div className="box">
+                <Image 
+                          src="/Image/Avanza.png"
+                          alt="Image"
+                width={355}
+                height={180}
+                    />
+                    </div>
+             </div>
+
+
+         </div>
+       </section>
+
+
+    )
+}
+export default Portfolio
